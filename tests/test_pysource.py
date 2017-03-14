mbukatov/pylatest

commit f43e47593d237b53190dc2b89889747f153ee49a

simplify processing of plain action directives
Instead of using docutils transformations hack as done with table
directives, plain directives now just creates action nodes directly
(without any transformation or pending nodes).

This siplifies the code for plain variants of directives and allows more
information to be discovered (eg. for the purpose of rstsource module),
but it also changes structure of resulting node tree a bit:

* plain directives would never produce any pending nodes
* action nodes are not wrapped into paragraph node anymore

Unfortunatelly, it also:

* break auto id feature for plain output

pylatest/rstsource.py
@@ -119,18 +119,6 @@ def _teststeps_condition(node):
         isinstance(node, pylatest.xdocutils.nodes.test_result_node))
 
 
-def _teststeps_condition_hack(node):
-    """
-    Traverse condition for filtering nodes of test steps directives.
-
-    This is a quick hack to overcome issue with ``_teststeps_condition()``.
-    """
-    # TODO: proper check (of at least transformation class) goes here
-    if isinstance(node, nodes.pending):
-        return True
-    return False
-
-
 def find_actions(rst_source):
     # parse rst_source string to get rst node tree
     nodetree = publish_doctree(source=rst_source)
@@ -158,7 +146,12 @@ def find_actions(rst_source):
             )
         if len(next_siblings) > 0:
             next_node = next_siblings[0]
-            end_line = next_node.children[0].line - 4
+            if next_node.tagname in ("test_step_node", "test_result_node"):
+                end_line = next_node.children[0].line - 4
+            elif next_node.tagname == "section":
+                end_line = next_node.line - 3
+            else:
+                end_line = next_node.line - 2
         else:
             # when there are no next sibling nodes (this is the last directive
             # node in given subtree), we need to get to go up in the node tree
@@ -241,8 +234,6 @@ def detect_docstring_sections(docstring):
     # try to count all pylatest step/result directives
     test_directive_count = 0
     for node in nodetree.traverse(_teststeps_condition):
-        test_directive_count += 1
-    for node in nodetree.traverse(_teststeps_condition_hack):
         test_directive_count += 1
 
     # try to detect header pseudo section (contains name and metadata)

tests/test_rstsource.py
@@ -441,7 +441,7 @@ class TestFindActions(unittest.TestCase):
     # nodes into particular action nodes
     # the only way to have this would include disabling transform phase during
     # rst parsing in pylatest.rstsource module
-    def _test_find_actions_mixed(self):
+    def test_find_actions_mixed(self):
         src = textwrap.dedent('''\
         This looks little fishy.
 

pylatest/xdocutils/directives.py
@@ -28,10 +28,43 @@ from docutils import nodes
 from docutils.parsers import rst
 
 from pylatest.xdocutils.transforms import TestStepsTableTransform
-from pylatest.xdocutils.transforms import TestStepsPlainTransform
 from pylatest.xdocutils.transforms import TestMetadataTableTransform
 from pylatest.xdocutils.transforms import TestMetadataPlainTransform
 import pylatest.xdocutils.nodes
+
+
+def arguments2action_id(arguments):
+    """
+    Converts agruments of Rst Directive into action id.
+
+    Note that *action* is couple of test step and result with the same
+    ``action_id``.
+    """
+    if len(arguments) == 0:
+        action_id = None
+    else:
+        action_id = int(arguments[0])
+    return action_id
+
+
+def parse_content(state, content, content_offset, options):
+    """
+    Parse content of a directive (via state.nexted_parse) into new anonymous
+    node element.
+    """
+    # first of all, parse text content of this directive
+    # into anonymous node element (can't be used directly in the tree)
+    content_node = nodes.Element()
+    if 'include' in options:
+        # include HACK: link to the referred test case
+        # TODO: use a proper referece (nested parse?)
+        # TODO: the final solutions is a working include though
+        ref_tc, ref_action = str(options['include']).split(":", 2)
+        text_content = "See {0}, action {1}".format(ref_tc, ref_action)
+        content_node += nodes.paragraph(text=text_content)
+    else:
+        state.nested_parse(content, content_offset, content_node)
+    return content_node
 
 
 class TestActionDirective(rst.Directive):
@@ -64,29 +97,20 @@ class TestActionDirective(rst.Directive):
         # with action_id == 1
         # note:
         # action is couple of test step and result with the same action_id
-        if len(self.arguments) == 0:
-            action_id = None
-        else:
-            action_id = int(self.arguments[0])
+        action_id = arguments2action_id(self.arguments)
+
         # first of all, parse text content of this directive
         # into anonymous node element (can't be used directly in the tree)
-        node = nodes.Element()
-        # include HACK: link to the referred test case
-        # TODO: use a proper referece (nested parse?)
-        # TODO: the final solutions is a working include though
-        if 'include' in self.options:
-            ref_tc, ref_action = str(self.options['include']).split(":", 2)
-            text_content = "See {0}, action {1}".format(ref_tc, ref_action)
-            node += nodes.paragraph(text=text_content)
-        else:
-            self.state.nested_parse(self.content, self.content_offset, node)
+        content_node = parse_content(
+            self.state, self.content, self.content_offset, self.options)
+
         # create new pending node, which:
         #  - holds actual data (parsed content of the directive)
         #  - references transform class which is concerned with this node.
         #  - name of the directive (test_step or test_result)
         pending = nodes.pending(self.transform_class)
         # add content into pending node
-        pending.details['nodes'] = node
+        pending.details['nodes'] = content_node
         pending.details['action_id'] = action_id
         pending.details['action_name'] = self.name
         # since pylatest transformation will process all pylatest pending nodes
@@ -96,6 +120,7 @@ class TestActionDirective(rst.Directive):
         if action_id == 1 and self.name == "test_step":
             # without this, transformer wouldn't know about this pending node
             self.state_machine.document.note_pending(pending)
+
         # and finally return the pending node as the only result
         return [pending]
 
@@ -120,13 +145,40 @@ class TestActionTableDirectiveAutoId(TestActionTableDirective):
     optional_arguments = 1
 
 
-class TestActionPlainDirective(TestActionDirective):
+class TestActionPlainDirective(rst.Directive):
     """
     Implementation of ``test_step`` and ``test_result`` directives for
     further processing, eg. checking particular part of resulting document.
     """
 
-    transform_class = TestStepsPlainTransform
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = False
+    has_content = True
+    option_spec = {
+        'include': str,
+        }
+
+    def run(self):
+        action_id = arguments2action_id(self.arguments)
+
+        # parse text content of this directive into anonymous node element
+        # (can't be used directly in the tree)
+        content_node = parse_content(
+            self.state, self.content, self.content_offset, self.options)
+
+        # create new action node (either ``test_steprgr_node`` or
+        # ``test_result_node``)
+        node_name = "{}_node".format(self.name)
+        action_node = getattr(pylatest.xdocutils.nodes, node_name)()
+
+        # add all content nodes into the new action node
+        for content in content_node:
+            action_node += content
+        action_node.attributes['action_id'] = action_id
+
+        # and finally return the action node as the only result
+        return [action_node]
 
 
 class TestActionPlainDirectiveAutoId(TestActionPlainDirective):

pylatest/xdocutils/transforms.py
@@ -189,34 +189,6 @@ class TestStepsTableTransform(TestStepsTransform):
         return table_node
 
 
-class TestStepsPlainTransform(TestStepsTransform):
-    """
-    Wrapp content from pending test step nodes in div element so that test
-    steps are adressable via xpath (works with html output only).
-    """
-
-    _node_names = ('test_step_node', 'test_result_node')
-    """
-    Class names of rst tree nodes (for html plain output) of both *test
-    step* and *test result* of a test action.
-    """
-
-    def _create_content(self):
-        p_node = nodes.paragraph()
-        for action_id, step_nodes, result_nodes in self._actions:
-            for action_nodes, node_name in \
-                    zip((step_nodes, result_nodes), self._node_names):
-                if action_nodes is None:
-                    continue
-                node = getattr(pylatest.xdocutils.nodes, node_name)()
-                # add all content nodes into step or result node
-                for c_node in action_nodes.details['nodes']:
-                    node += c_node
-                node.attributes['action_id'] = action_id
-                p_node += node
-        return p_node
-
-
 class TestMetadataTransform(PylatestTransform):
     """
     Base trasformation class for test metadata directive.

tests/test_export.py
@@ -115,8 +115,8 @@ class TestRst2HtmlBodyTree(unittest.TestCase):
         # TODO: if the pylatest directive is unknown, error goes to stderr!
         html_body = export.rst2htmlbodytree(rst_content)
         # construct expected tree
-        html_body_expected, p_el = get_singleparagraph_body_tree()
-        div_el = add_action_div(p_el, '1', 'step')
+        html_body_expected = get_empty_body_tree()
+        div_el = add_action_div(html_body_expected, '1', 'step')
         div_el.text = '\nThis is just a test.\n'
         self.assertEqual(
             etree.tostring(html_body),

tests/test_xdocutils.py
@@ -105,10 +105,9 @@ class TestTestActionsPlain(TestBasePlain):
         rst_input = '.. test_step:: 1'
         exp_result = textwrap.dedent('''\
         <div class="document">
-        <p><div action_id="1" action_name="step" class="pylatest_action">
+        <div action_id="1" action_name="step" class="pylatest_action">
 
         </div>
-        </p>
         </div>
         ''')
         self.check_html_body(rst_input, exp_result)
@@ -121,10 +120,9 @@ class TestTestActionsPlain(TestBasePlain):
         ''')
         exp_result = textwrap.dedent('''\
         <div class="document">
-        <p><div action_id="1" action_name="step" class="pylatest_action">
+        <div action_id="1" action_name="step" class="pylatest_action">
         Ceterum censeo Carthaginem esse delendam.
         </div>
-        </p>
         </div>
         ''')
         self.check_html_body(rst_input, exp_result)
@@ -141,13 +139,12 @@ class TestTestActionsPlain(TestBasePlain):
         ''')
         exp_result = textwrap.dedent('''\
         <div class="document">
-        <p><div action_id="1" action_name="step" class="pylatest_action">
+        <div action_id="1" action_name="step" class="pylatest_action">
         Ceterum censeo Carthaginem esse delendam.
         </div>
         <div action_id="1" action_name="result" class="pylatest_action">
         This city is no more ... it has ceased to be ...
         </div>
-        </p>
         </div>
         ''')
         self.check_html_body(rst_input, exp_result)
@@ -172,7 +169,7 @@ class TestTestActionsPlain(TestBasePlain):
         ''')
         exp_result = textwrap.dedent('''\
         <div class="document">
-        <p><div action_id="1" action_name="step" class="pylatest_action">
+        <div action_id="1" action_name="step" class="pylatest_action">
         Ceterum censeo Carthaginem esse delendam.
         </div>
         <div action_id="1" action_name="result" class="pylatest_action">
@@ -184,7 +181,6 @@ class TestTestActionsPlain(TestBasePlain):
         <div action_id="2" action_name="result" class="pylatest_action">
         Result bar.
         </div>
-        </p>
         </div>
         ''')
         self.check_html_body(rst_input, exp_result)
@@ -215,13 +211,12 @@ class TestTestActionsPlainAutoId(TestTestActionsPlain):
         ''')
         exp_result = textwrap.dedent('''\
         <div class="document">
-        <p><div action_id="1" action_name="step" class="pylatest_action">
+        <div action_id="1" action_name="step" class="pylatest_action">
         Ceterum censeo Carthaginem esse delendam.
         </div>
         <div action_id="1" action_name="result" class="pylatest_action">
         This city is no more ... it has ceased to be ...
         </div>
-        </p>
         </div>
         ''')
         self.check_html_body(rst_input, exp_result)
@@ -246,7 +241,7 @@ class TestTestActionsPlainAutoId(TestTestActionsPlain):
         ''')
         exp_result = textwrap.dedent('''\
         <div class="document">
-        <p><div action_id="1" action_name="step" class="pylatest_action">
+        <div action_id="1" action_name="step" class="pylatest_action">
         Ceterum censeo Carthaginem esse delendam.
         </div>
         <div action_id="1" action_name="result" class="pylatest_action">
@@ -258,7 +253,6 @@ class TestTestActionsPlainAutoId(TestTestActionsPlain):
         <div action_id="2" action_name="result" class="pylatest_action">
         Result bar.
         </div>
-        </p>
         </div>
         ''')
         self.check_html_body(rst_input, exp_result)

tests/test_xdocutils_directives.py
@@ -91,14 +91,7 @@ class TestTestActionsDirectivePlain(TestBasePlain):
         rst_input = '.. test_step:: 1'
         exp_result = textwrap.dedent('''\
         <document source="testparse() method">
-            <pending>
-                .. internal attributes:
-                     .transform: pylatest.xdocutils.transforms.TestStepsPlainTransform
-                     .details:
-                       action_id: 1
-                       action_name: 'test_step'
-                       nodes:
-                         <Element>
+            <test_step_node action_id="1">
         ''')
         self.check_directive(rst_input, exp_result)
 
@@ -119,14 +112,7 @@ class TestTestActionsDirectivePlain(TestBasePlain):
         rst_input = '.. test_result:: 1'
         exp_result = textwrap.dedent('''\
         <document source="testparse() method">
-            <pending>
-                .. internal attributes:
-                     .transform: pylatest.xdocutils.transforms.TestStepsPlainTransform
-                     .details:
-                       action_id: 1
-                       action_name: 'test_result'
-                       nodes:
-                         <Element>
+            <test_result_node action_id="1">
         ''')
         self.check_directive(rst_input, exp_result)
 
@@ -138,16 +124,9 @@ class TestTestActionsDirectivePlain(TestBasePlain):
         ''')
         exp_result = textwrap.dedent('''\
         <document source="testparse() method">
-            <pending>
-                .. internal attributes:
-                     .transform: pylatest.xdocutils.transforms.TestStepsPlainTransform
-                     .details:
-                       action_id: 7
-                       action_name: 'test_step'
-                       nodes:
-                         <Element>
-                             <paragraph>
-                                 Some content.
+            <test_step_node action_id="7">
+                <paragraph>
+                    Some content.
         ''')
         self.check_directive(rst_input, exp_result)
 
@@ -159,16 +138,9 @@ class TestTestActionsDirectivePlain(TestBasePlain):
         ''')
         exp_result = textwrap.dedent('''\
         <document source="testparse() method">
-            <pending>
-                .. internal attributes:
-                     .transform: pylatest.xdocutils.transforms.TestStepsPlainTransform
-                     .details:
-                       action_id: 7
-                       action_name: 'test_result'
-                       nodes:
-                         <Element>
-                             <paragraph>
-                                 Some content.
+            <test_result_node action_id="7">
+                <paragraph>
+                    Some content.
         ''')
         self.check_directive(rst_input, exp_result)
 

commit d505b09ff71ea375d278d5ec0fbea43249bb8ca8

fix detection of section fragments
During refactoring, I ommited a special case (which I'm addressing here
again): sections fragments, rst source which contains just title and
some content, which represent a mere fragment, particular section from a
test case document.

This commit fixes all remaining unit test failures with the exception of
auto id feature for plain output (as noted and broken in commit
f43e47593d237b53190dc2b89889747f153ee49a).

pylatest/rstsource.py
@@ -100,6 +100,10 @@ def find_sections(rst_source):
     """
     # parse rst_source string to get rst node tree
     nodetree = publish_doctree(source=rst_source)
+    # shortcut: immediatelly return for empty doc (so that we can assume
+    # nonempty nodetree later)
+    if len(rst_source) == 0 or len(nodetree) == 0:
+        return []
     # look for rst sections
     sections = []
     prev_section = None
@@ -131,6 +135,17 @@ def find_sections(rst_source):
         else:
             section = RstSection(None, 1, sections[0].start_line - 2)
             sections.append(section)
+    # a special case: this is necessary to be able to find sections fragments
+    # (rst source which contains just title and some content) - as long as rst
+    # parser is concerned, it's a rst document, but wrt pylatest test case
+    # document, it represent a mere fragment, particular section from a test
+    # case
+    if len(sections) == 0 and \
+       nodetree[0].tagname == "title" and \
+       rst_source[0].isalpha():
+        title = nodetree[0].astext()
+        section = RstSection(title, 1, get_last_line_num(rst_source))
+        sections.append(section)
     return sections
 
 

tests/test_pysource.py
@@ -248,6 +248,110 @@ class TestTestCaseDocFragments(unittest.TestCase):
         self.assertTrue(TestCaseDoc.STEPS in doc.sections)
         self.assertTrue(TestCaseDoc.TEARD in doc.sections)
 
+    def test_docfragments_build_doc_multiple_fragmented(self):
+        rst_fragments = [
+            textwrap.dedent("""\
+            Hello World Test Case
+            *********************
+
+            .. test_metadata:: author foo@example.com
+            .. test_metadata:: date 2015-11-06
+            .. test_metadata:: comment This is here just to test metadata processing.
+            """),
+            textwrap.dedent("""\
+            Description
+            ===========
+
+            This is just demonstration of usage of pylatest rst directives and expected
+            structure of rst document.
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.
+            Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec
+            consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero
+            egestas mattis sit amet vitae augue.
+
+            See :BZ:`439858` for more details.
+            """),
+            textwrap.dedent("""\
+            .. test_step:: 1
+
+                List files in the volume: ``ls -a /mnt/helloworld``
+            """),
+            textwrap.dedent("""\
+            .. test_result:: 1
+
+                There are no files, output should be empty.
+            """),
+            textwrap.dedent("""\
+            .. test_step:: 2
+
+                Donec et mollis dolor::
+
+                    $ foo --extra sth
+                    $ bar -vvv
+            """),
+            textwrap.dedent("""\
+            .. test_result:: 2
+
+                Maecenas congue ligula ac quam viverra nec
+                consectetur ante hendrerit.
+            """),
+            textwrap.dedent("""\
+            .. test_step:: 3
+
+                This one has no matching test result.
+            """),
+            textwrap.dedent("""\
+            .. test_result:: 4
+
+                And this result has no test step.
+            """),
+            textwrap.dedent("""\
+            Setup
+            =====
+
+            #. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam
+               lectus. Sed sit amet ipsum mauris.
+
+            #. Use lvm disk paritioning and Leave 10G free space in volume
+               called ``lv_helloword``.
+
+            #. When the system is installed, format ``lv_helloword`` volume with
+               brtfs using ``--super --special --options``.
+
+            #. Mount it on a client::
+
+                # mount -t btrfs /dev/mapper/vg_fedora/lv_helloword /mnt/helloworld
+
+            #. Ceterum censeo, lorem ipsum::
+
+                # dnf install foobar
+                # systemctl enable foobard
+            """),
+            textwrap.dedent("""\
+            Teardown
+            ========
+
+            #. Lorem ipsum dolor sit amet: ``rm -rf /mnt/helloworld``.
+
+            #. Umount and remove ``lv_helloword`` volume.
+
+            #. The end.
+            """),
+            ]
+        for index, fragment in enumerate(rst_fragments):
+            # line number have to be unique
+            lineno = index*100 + 2
+            self.fragments.add_fragment(fragment, lineno=lineno)
+        self.assertEqual(len(self.fragments), len(rst_fragments))
+        doc = self.fragments.build_doc()
+        self.assertFalse(doc.is_empty())
+        self.assertTrue(TestCaseDoc.DESCR in doc.sections)
+        self.assertTrue(TestCaseDoc.SETUP in doc.sections)
+        self.assertTrue(TestCaseDoc.STEPS in doc.sections)
+        self.assertTrue(TestCaseDoc.TEARD in doc.sections)
+        self.assertTrue(TestCaseDoc._HEAD in doc.sections)
+
 
 class TestExtractDocumentFragments(unittest.TestCase):
     """

tests/test_rstsource.py
@@ -48,8 +48,13 @@ class TestFindSections(unittest.TestCase):
         ***********
 
         There are no sections, just a document title.
+        But in this case, it's evaluated as a section, so that
+        we are able to detect section fragments.
         ''')
-        self.assertEqual(rstsource.find_sections(src), [])
+        exp_sections = [
+            rstsource.RstSection("Hello World", 1, 6),
+            ]
+        self.assertEqual(rstsource.find_sections(src), exp_sections)
 
     def test_find_sections_docwithoutsections_two(self):
         src = textwrap.dedent('''\