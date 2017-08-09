dcce3497877e5c9298a2ad74c44a33bb6a8d0ee2
Martin Bukatovic <mbukatov@redhat.com>
2017-08-09 15:30:02 +0200

    setup.py: set 3.6 as supported python 3 version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         'Topic :: Text Processing :: Markup',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         ],
     packages=find_packages(exclude=['doc', 'tests']),
     install_requires=['docutils', 'lxml', 'sphinx'],
