d05cc6bcd2580174d55649562b6265c25ab76af2
Martin Bukatovic <mbukatov@redhat.com>
2017-08-15 17:48:39 +0200

    setup.py: specify minimal sphinx version
    Solely based on unit test results: 1.4.6 is the oldest version of sphinx
    for which all tests pass.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         ],
     packages=find_packages(exclude=['doc', 'tests']),
-    install_requires=['docutils', 'lxml', 'sphinx'],
+    install_requires=['docutils', 'lxml', 'sphinx>=1.4.6'],
     # TODO: make this work with git (and remove MANIFEST.in?)
     # setup_requires=['setuptools_scm'],
     # use_scm_version=True,
