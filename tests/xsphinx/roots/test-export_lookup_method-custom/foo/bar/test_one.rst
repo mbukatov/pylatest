da40bc89e5cf5c4632acac0a23f914669fc3fe7c
Martin Bukatovic <martin.bukatovic@gmail.com>
2018-03-30 23:00:06 +0200

    extend test_export_lookup_method with dir tree

---
+++ tests/xsphinx/roots/test-export_lookup_method-custom/foo/bar/test_one.rst
@@ -0,0 +1,11 @@
+Test One
+********
+
+:author: joe@example.com
+
+Test Steps
+==========
+
+.. test_action::
+   :step: Do this.
+   :result: And this should happen.
--- tests/xsphinx/test_export_lookup_method.py
+++ tests/xsphinx/test_export_lookup_method.py
@@ -11,9 +11,13 @@ def test_lookup_method_custom_id_value(app, status, warning):
     # parse test case document builds
     foo_tree = xmlparse_testcase(app.outdir, "test_foo", "xmlexport")
     bar_tree = xmlparse_testcase(app.outdir, "test_bar", "xmlexport")
+    one_tree = xmlparse_testcase(app.outdir, "foo/bar/test_one", "xmlexport")
+    two_tree = xmlparse_testcase(app.outdir, "foo/bar/test_two", "xmlexport")
     # check test case id values (generated based on doc name)
     assert foo_tree.xpath('/testcases/testcase/@id') == ["/test_foo"]
     assert bar_tree.xpath('/testcases/testcase/@id') == ["/test_bar"]
+    assert one_tree.xpath('/testcases/testcase/@id') == ["/foo/bar/test_one"]
+    assert two_tree.xpath('/testcases/testcase/@id') == ["/foo/bar/test_two"]
 
 
 @pytest.mark.sphinx('xmlexport', testroot='export_lookup_method-custom')
@@ -22,6 +26,9 @@ def test_lookup_method_custom_properties(app, status, warning):
     # parse test case document builds
     foo_tree = xmlparse_testcase(app.outdir, "test_foo", "xmlexport")
     bar_tree = xmlparse_testcase(app.outdir, "test_bar", "xmlexport")
+    one_tree = xmlparse_testcase(app.outdir, "foo/bar/test_one", "xmlexport")
+    two_tree = xmlparse_testcase(app.outdir, "foo/bar/test_two", "xmlexport")
     # check that properties are set as expected
-    assert foo_tree.xpath("/testcases/properties/property[@name='lookup-method']/@value") == ["custom"]
-    assert bar_tree.xpath("/testcases/properties/property[@name='lookup-method']/@value") == ["custom"]
+    for tree in (foo_tree, bar_tree, one_tree, two_tree):
+        val_xp = "/testcases/properties/property[@name='lookup-method']/@value"
+        assert tree.xpath(val_xp) == ["custom"]
